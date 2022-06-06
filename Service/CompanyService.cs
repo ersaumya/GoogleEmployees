550b2bf4dd9bdc5976aaf5db3d6a714bfa019977
ersaumya <ersaumyarout@gmail.com>
2022-06-06 21:21:49 +0530

    Get By Id Http request

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -33,10 +33,16 @@ namespace Service
                 var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
                 return companiesDto;
-                
-            
-           
-            
+
         }
+
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges);
+            //Check if the company is null 
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return companyDto;
+        }
+
     }
 }
