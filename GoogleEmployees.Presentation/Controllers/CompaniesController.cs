ab22a02a848c194ebd50551463d83d95c50acdf4
ersaumya <ersaumyarout@gmail.com>
2022-06-07 17:57:05 +0530

    Creating collection of resource

--- GoogleEmployees.Presentation/Controllers/CompaniesController.cs
+++ GoogleEmployees.Presentation/Controllers/CompaniesController.cs
@@ -42,6 +42,19 @@ namespace GoogleEmployees.Presentation.Controllers
             createdCompany);
         }
 
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        {
+            var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
+            return Ok(companies);
+        }
+
+        [HttpPost("collection")]
+        public IActionResult CreateCompanyCollection([FromBody]IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            var result =_service.CompanyService.CreateCompanyCollection(companyCollection);
+            return CreatedAtRoute("CompanyCollection", new { result.ids },result.companies);
+        }
 
 
     }
