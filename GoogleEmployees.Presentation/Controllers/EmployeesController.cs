c43cf3166303896c4df57da6516cc6fae5d36f7c
ersaumya <ersaumyarout@gmail.com>
2022-06-07 11:26:41 +0530

    Get single employee by id

--- GoogleEmployees.Presentation/Controllers/EmployeesController.cs
+++ GoogleEmployees.Presentation/Controllers/EmployeesController.cs
@@ -14,12 +14,20 @@ namespace GoogleEmployees.Presentation.Controllers
     {
         private readonly IServiceManager _service;
         public EmployeesController(IServiceManager service) => _service = service;
+
         [HttpGet]
         public IActionResult GetEmployeesForCompany(Guid companyId)
         {
             var employees = _service.EmployeeService.GetEmployees(companyId, trackChanges:
             false);
             return Ok(employees);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var employee = _service.EmployeeService.GetEmployee(companyId, id,trackChanges: false);
+            return Ok(employee);
         }
 
     }
--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -10,6 +10,8 @@ namespace Service.Contracts
     public interface IEmployeeService
     {
         IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+        EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
+
 
     }
 }
