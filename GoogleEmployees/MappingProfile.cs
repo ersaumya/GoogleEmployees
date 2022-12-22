12c7240e0ba827d2fc3138054bbabc056b15deeb
SOUMYA\ersau <ersaumyarout@gmail.com>
2022-12-22 11:27:30 +0530

    Added Dto and mapping for creating child resource while updating parent resource

--- GoogleEmployees/MappingProfile.cs
+++ GoogleEmployees/MappingProfile.cs
@@ -14,6 +14,7 @@ namespace GoogleEmployees
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
             CreateMap<EmployeeForUpdateDto, Employee>();
+            CreateMap<CompanyForUpdateDto, Company>();
         }
     }
 }
