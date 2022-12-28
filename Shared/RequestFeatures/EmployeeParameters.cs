92efdf562e5d3cddf9616749bb51b931d864ff62
SOUMYA\ersau <ersaumyarout@gmail.com>
2022-12-28 13:09:03 +0530

    implemented searching with employee name

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -8,8 +8,12 @@ namespace Shared.RequestFeatures
 {
     public class EmployeeParameters:RequestParameters
     {
+        //For Filtering
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
+
+        //For searching
+        public string? SearchTerm { get; set; }
     }
 }
