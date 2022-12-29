056b8b5244c32497f900a1f1b2d24bf122f500d1
SOUMYA\ersau <ersaumyarout@gmail.com>
2022-12-29 13:06:23 +0530

    HTTP Header Versioning

--- GoogleEmployees.Presentation/Controllers/CompaniesV2Controller.cs
+++ GoogleEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 namespace GoogleEmployees.Presentation.Controllers
 {
     [ApiVersion("2.0")]
-    [Route("api/{v:apiversion}/companies")]
+    //[Route("api/{v:apiversion}/companies")]
+    [Route("api/companies")]
     [ApiController]
     public class CompaniesV2Controller : ControllerBase
     {
