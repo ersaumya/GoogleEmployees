235e605a766f9180fbfce9a672fc8bbbc89e6dd2
SOUMYA\ersau <ersaumyarout@gmail.com>
2023-01-10 10:58:06 +0530

    added xml comment to the api using swagger

--- GoogleEmployees.Presentation/Controllers/CompaniesController.cs
+++ GoogleEmployees.Presentation/Controllers/CompaniesController.cs
@@ -23,6 +23,10 @@ namespace GoogleEmployees.Presentation.Controllers
         private readonly IServiceManager _service;
         public CompaniesController(IServiceManager service) => _service = service;
 
+        /// <summary>
+        /// Gets the list of all companies
+        /// </summary>
+        /// <returns>The companies list</returns>
         [HttpGet(Name = "GetCompanies")]
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> GetCompanies()
@@ -42,9 +46,19 @@ namespace GoogleEmployees.Presentation.Controllers
             var company = await _service.CompanyService.GetCompanyAsync(id, trackChanges: false);
             return Ok(company);
         }
-
+        /// <summary>
+        /// Creates a newly created company
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns>A newly created company</returns>
+        /// <response code="201">Returns the newly created item</response> 
+        /// <response code="400">If the item is null</response> 
+        /// <response code="422">If the model is invalid</response>
         [HttpPost(Name = "CreateCompany")]
         [ServiceFilter(typeof(ValidationFilterAttribute))]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(422)]
         public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
             var createdCompany = await _service.CompanyService.CreateCompanyAsync(company);
--- GoogleEmployees/Extensions/ServiceExtensions.cs
+++ GoogleEmployees/Extensions/ServiceExtensions.cs
@@ -186,6 +186,11 @@ namespace GoogleEmployees.Extensions
 
                 });
                 s.SwaggerDoc("v2", new OpenApiInfo { Title = "GoogleEmployee API", Version = "v2" });
+
+                var xmlFile = $"{typeof(Presentation.AssemblyReference).Assembly.GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+
                 s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     In = ParameterLocation.Header,
